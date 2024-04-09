ea4b84768f4c135aa5c9f6e3d28ac750bb3701e1
phamdophuc2180601177 <phamdophuc12345@gmail.com>
2024-04-09 21:22:52 +0700

    [add] lab-02 playfair

--- lab-02/cipher/api.py
+++ lab-02/cipher/api.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from caesar.caesar_cipher import CaesarCipher
 from vigenere import VigenereCipher
-from railfence import RailFenceCipher 
+from railfence import RailFenceCipher
+from playfair import PlayFairCipher
+ 
 app = Flask(__name__)
 
 #CAESAR CIPHER ALGORITHM
 caeser_cipher = CaesarCipher()
 vigenere_cipher = VigenereCipher()
 railfence_cipher = RailFenceCipher()
-
+playfair_cipher = PlayFairCipher()
 
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
@@ -59,6 +61,34 @@ def decrypt():
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
+
+#PLAY FAIR
+@app.route('/api/playfair/creatematrix', methods=["POST"])
+def playfair_creatematrix():
+    data = request.json
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    return jsonify({'playfair_matrix': playfair_matrix})
+
+@app.route('/api/playfair/encrypt', methods=["POST"])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/playfair/decrypt', methods=["POST"])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text': decrypted_text}) 
+
+
 #main function
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
